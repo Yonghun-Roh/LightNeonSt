dc698737556024cbf8bd06f114ce6654f98173cc
Ro YongHun <mogi3206@naver.com>
2022-11-08 16:19:58 +0900

    yelloLabel add

---
+++ LightNeonSt/SettingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingViewController.swift
+//  LightNeonSt
+//
+//  Created by Yonghun Roh on 2022/11/08.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
